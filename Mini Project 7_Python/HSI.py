5e8df983de00bf08f25e47613adb85f51d0cecd4
Bui Minh Thang <alexanderbui22102003@gmail.com>
2025-02-07 18:57:18 +0700

    Add files via upload

---
+++ Mini Project 7_Python/HSI.py
@@ -0,0 +1,73 @@
+import cv2
+from PIL import Image
+import numpy as np
+import math
+
+def ChuyenDoiRGBSangHSV(imgPIL):
+    Hue = Image.new(imgPIL.mode, imgPIL.size)
+    Saturation = Image.new(imgPIL.mode, imgPIL.size)
+    Value = Image.new(imgPIL.mode, imgPIL.size)
+    HSV = Image.new(imgPIL.mode, imgPIL.size)
+
+    width = imgPIL.size[0]
+    heigh = imgPIL.size[1]
+
+    for x in range(width):
+        for y in range(heigh):
+            R, G, B = imgPIL.getpixel((x,y))
+            
+            if R == 0 and G == 0 and B == 0 and (R-G)*(R-G)+(R-B)*(G-B) == 0:
+                Hue.putpixel((x, y), (0, 0, 0))
+                Saturation.putpixel((x, y), (0, 0, 0))
+                Value.putpixel((x, y), (0, 0, 0))
+                HSV.putpixel((x, y), (0, 0, 0))
+            else:
+            
+                t1 = ((R-G)+(R-B))/2
+                t2 = math.sqrt(((R-G)*(R-G)+(R-B)*(G-B)))
+                if t2 != 0:
+                    t = t1/t2
+                    theta = math.acos(t)## rad
+
+                    HueC = 0
+                    if (B <= G):
+                        HueC = theta
+                    else:
+                        HueC = 2*math.pi - theta
+                
+                H = np.uint8((HueC*180/math.pi)*255/360)#############################
+
+                Min = min(R,G,B)
+                S = 1 -3*Min/(R+G+B)
+                S = np.uint8(S*255)
+
+                V = np.uint8(max(R,G,B))
+                
+                Hue.putpixel((x,y),(H,H,H))
+                Saturation.putpixel((x,y), (S,S,S))
+                Value.putpixel((x,y),(V,V,V))
+                HSV.putpixel((x,y),(V, S, 0))
+
+    HueRGB = np.array(Hue)
+    SaturationRGB = np.array(Saturation)
+    ValueRGB = np.array(Value) 
+    HSVRGB= np.array(HSV)
+
+    return HueRGB, SaturationRGB, ValueRGB, HSVRGB
+
+
+filehinh = r'E:\Machine Vision\XLA_01FIE\lena_color.png'
+img = cv2.imread(filehinh, cv2.IMREAD_COLOR)
+imgPIL = Image.open(filehinh)
+imgPIL = imgPIL.convert('RGB')
+
+
+Hue, Saturation, Value, HSVImg = ChuyenDoiRGBSangHSV(imgPIL)
+
+cv2.imshow('RGB_original', img)
+cv2.imshow('Hue', Hue)
+cv2.imshow('Saturation', Saturation)
+cv2.imshow('Intensity', Value)
+cv2.imshow('HSI Img', HSVImg)
+cv2.waitKey(0)
+cv2.destroyAllWindows
