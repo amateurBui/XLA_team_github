b2e134eb5ea97b8f7026e27f9994aff85398a8e9
Nguyễn Trung Nhân <112737822+jeremynguyenn@users.noreply.github.com>
2025-02-09 21:31:32 +0700

    miniproject9

---
+++ 21146033_NGUYENTRUNGNHAN_mini-project9/bài 9/bài 9/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace bài_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBoxKenhX = new System.Windows.Forms.PictureBox();
+            this.picBoxKenhY = new System.Windows.Forms.PictureBox();
+            this.picBoxKenhZ = new System.Windows.Forms.PictureBox();
+            this.picBoxKenhXYZ = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.picBoxHinhGoc = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhZ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhXYZ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHinhGoc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxKenhX
+            // 
+            this.picBoxKenhX.Location = new System.Drawing.Point(287, 37);
+            this.picBoxKenhX.Name = "picBoxKenhX";
+            this.picBoxKenhX.Size = new System.Drawing.Size(256, 256);
+            this.picBoxKenhX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxKenhX.TabIndex = 0;
+            this.picBoxKenhX.TabStop = false;
+            // 
+            // picBoxKenhY
+            // 
+            this.picBoxKenhY.Location = new System.Drawing.Point(564, 37);
+            this.picBoxKenhY.Name = "picBoxKenhY";
+            this.picBoxKenhY.Size = new System.Drawing.Size(256, 256);
+            this.picBoxKenhY.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxKenhY.TabIndex = 1;
+            this.picBoxKenhY.TabStop = false;
+            // 
+            // picBoxKenhZ
+            // 
+            this.picBoxKenhZ.Location = new System.Drawing.Point(840, 37);
+            this.picBoxKenhZ.Name = "picBoxKenhZ";
+            this.picBoxKenhZ.Size = new System.Drawing.Size(256, 256);
+            this.picBoxKenhZ.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxKenhZ.TabIndex = 2;
+            this.picBoxKenhZ.TabStop = false;
+            // 
+            // picBoxKenhXYZ
+            // 
+            this.picBoxKenhXYZ.Location = new System.Drawing.Point(1117, 37);
+            this.picBoxKenhXYZ.Name = "picBoxKenhXYZ";
+            this.picBoxKenhXYZ.Size = new System.Drawing.Size(256, 256);
+            this.picBoxKenhXYZ.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxKenhXYZ.TabIndex = 3;
+            this.picBoxKenhXYZ.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(283, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Kênh X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label2.Location = new System.Drawing.Point(1113, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kênh XYZ:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label3.Location = new System.Drawing.Point(560, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kênh Y:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label4.Location = new System.Drawing.Point(836, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Kênh Z:";
+            // 
+            // picBoxHinhGoc
+            // 
+            this.picBoxHinhGoc.Location = new System.Drawing.Point(12, 37);
+            this.picBoxHinhGoc.Name = "picBoxHinhGoc";
+            this.picBoxHinhGoc.Size = new System.Drawing.Size(256, 256);
+            this.picBoxHinhGoc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxHinhGoc.TabIndex = 10;
+            this.picBoxHinhGoc.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label5.Location = new System.Drawing.Point(12, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 22);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "HinhGocRGB:";
+            // 
+            // Button
+            // 
+            this.Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button.ForeColor = System.Drawing.Color.OrangeRed;
+            this.Button.Location = new System.Drawing.Point(574, 337);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(178, 70);
+            this.Button.TabIndex = 12;
+            this.Button.Text = "RGB to XYZ";
+            this.Button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1484, 461);
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.picBoxHinhGoc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picBoxKenhXYZ);
+            this.Controls.Add(this.picBoxKenhZ);
+            this.Controls.Add(this.picBoxKenhY);
+            this.Controls.Add(this.picBoxKenhX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhZ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhXYZ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHinhGoc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxKenhX;
+        private System.Windows.Forms.PictureBox picBoxKenhY;
+        private System.Windows.Forms.PictureBox picBoxKenhZ;
+        private System.Windows.Forms.PictureBox picBoxKenhXYZ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox picBoxHinhGoc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Button;
+    }
+}
+
---
+++ 21146033_NGUYENTRUNGNHAN_mini-project9/bài 9/bài 9/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bài_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap HinhGoc = new Bitmap(@"D:\XLA\lena_color.png");
+            picBoxHinhGoc.Image = HinhGoc;
+            List<Bitmap> XYZ= ChuyenDoiRGBSangXYZ(HinhGoc);
+            Button.Click += Button_Click;
+        }
+        public List<Bitmap> ChuyenDoiRGBSangXYZ(Bitmap hinhgoc)
+        {
+            List<Bitmap> XYZ = new List<Bitmap>();
+            Bitmap KX = new Bitmap(hinhgoc.Width, hinhgoc.Height);
+            Bitmap KY = new Bitmap(hinhgoc.Width, hinhgoc.Height);
+            Bitmap KZ = new Bitmap(hinhgoc.Width, hinhgoc.Height);
+            Bitmap imgXYZ = new Bitmap(hinhgoc.Width, hinhgoc.Height);
+            for (int x = 0; x < hinhgoc.Width; x++)
+                for (int y = 0; y < hinhgoc.Height; y++)
+                {
+                    Color pixel = hinhgoc.GetPixel(x, y);
+                    double R = pixel.R;
+                    double G = pixel.G;
+                    double B = pixel.B;
+
+                    double X = (R * 0.4124564 + G * 0.3575761 + B * 0.1804375);
+                    double Y = (R * 0.2126729 + G * 0.7151522 + B * 0.0721750);
+                    double Z = (R * 0.0193339 + G * 0.1191920 + B * 0.9503041);
+
+                    KX.SetPixel(x, y, Color.FromArgb((byte)(X), (byte)(X), (byte)(X)));
+                    KY.SetPixel(x, y, Color.FromArgb((byte)(Y), (byte)(Y), (byte)(Y)));
+                    KZ.SetPixel(x, y, Color.FromArgb((byte)(Z), (byte)(Z), (byte)(Z)));
+                    imgXYZ.SetPixel(x, y, Color.FromArgb((byte)(X), (byte)(Y), (byte)(Z)));
+                }
+            XYZ.Add(KX);
+            XYZ.Add(KY);
+            XYZ.Add(KZ);
+            XYZ.Add(imgXYZ);
+            return XYZ;
+        }
+            private void Button_Click(object sender, EventArgs e)
+            {
+            Bitmap HinhGoc = new Bitmap(@"D:\XLA\lena_color.png");
+            picBoxHinhGoc.Image = HinhGoc;
+            List<Bitmap> XYZ = ChuyenDoiRGBSangXYZ(HinhGoc);
+            picBoxKenhX.Image = XYZ[0];
+            picBoxKenhY.Image = XYZ[1];
+            picBoxKenhZ.Image = XYZ[2];
+            picBoxKenhXYZ.Image = XYZ[3];
+
+        }    
+    }
+}    
+
