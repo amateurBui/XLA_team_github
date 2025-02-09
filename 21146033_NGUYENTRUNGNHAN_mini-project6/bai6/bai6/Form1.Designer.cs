c42dd784937fa62a8cfdd8825440075949c7164d
Nguyễn Trung Nhân <112737822+jeremynguyenn@users.noreply.github.com>
2025-02-09 21:19:36 +0700

    miniproject6

---
+++ 21146033_NGUYENTRUNGNHAN_mini-project6/bai6/bai6/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace bai6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBoxHinhGoc = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picBoxKenhMauCyan = new System.Windows.Forms.PictureBox();
+            this.picBoxKenhMauMagenta = new System.Windows.Forms.PictureBox();
+            this.picBoxKenhMauYellow = new System.Windows.Forms.PictureBox();
+            this.picBoxKenhMauBlack = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHinhGoc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhMauCyan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhMauMagenta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhMauYellow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhMauBlack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxHinhGoc
+            // 
+            this.picBoxHinhGoc.Location = new System.Drawing.Point(12, 44);
+            this.picBoxHinhGoc.Name = "picBoxHinhGoc";
+            this.picBoxHinhGoc.Size = new System.Drawing.Size(300, 300);
+            this.picBoxHinhGoc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxHinhGoc.TabIndex = 0;
+            this.picBoxHinhGoc.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hình Gốc:";
+            // 
+            // picBoxKenhMauCyan
+            // 
+            this.picBoxKenhMauCyan.Location = new System.Drawing.Point(318, 44);
+            this.picBoxKenhMauCyan.Name = "picBoxKenhMauCyan";
+            this.picBoxKenhMauCyan.Size = new System.Drawing.Size(300, 300);
+            this.picBoxKenhMauCyan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxKenhMauCyan.TabIndex = 2;
+            this.picBoxKenhMauCyan.TabStop = false;
+            // 
+            // picBoxKenhMauMagenta
+            // 
+            this.picBoxKenhMauMagenta.Location = new System.Drawing.Point(624, 44);
+            this.picBoxKenhMauMagenta.Name = "picBoxKenhMauMagenta";
+            this.picBoxKenhMauMagenta.Size = new System.Drawing.Size(300, 300);
+            this.picBoxKenhMauMagenta.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxKenhMauMagenta.TabIndex = 3;
+            this.picBoxKenhMauMagenta.TabStop = false;
+            // 
+            // picBoxKenhMauYellow
+            // 
+            this.picBoxKenhMauYellow.Location = new System.Drawing.Point(930, 44);
+            this.picBoxKenhMauYellow.Name = "picBoxKenhMauYellow";
+            this.picBoxKenhMauYellow.Size = new System.Drawing.Size(300, 300);
+            this.picBoxKenhMauYellow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxKenhMauYellow.TabIndex = 4;
+            this.picBoxKenhMauYellow.TabStop = false;
+            // 
+            // picBoxKenhMauBlack
+            // 
+            this.picBoxKenhMauBlack.Location = new System.Drawing.Point(1236, 44);
+            this.picBoxKenhMauBlack.Name = "picBoxKenhMauBlack";
+            this.picBoxKenhMauBlack.Size = new System.Drawing.Size(300, 300);
+            this.picBoxKenhMauBlack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxKenhMauBlack.TabIndex = 5;
+            this.picBoxKenhMauBlack.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kênh màu Cyan:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(621, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kênh màu Magenta:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(927, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Kênh màu Yellow:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1233, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Kênh màu Black:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1984, 1061);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.picBoxKenhMauBlack);
+            this.Controls.Add(this.picBoxKenhMauYellow);
+            this.Controls.Add(this.picBoxKenhMauMagenta);
+            this.Controls.Add(this.picBoxKenhMauCyan);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picBoxHinhGoc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxHinhGoc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhMauCyan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhMauMagenta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhMauYellow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxKenhMauBlack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxHinhGoc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picBoxKenhMauCyan;
+        private System.Windows.Forms.PictureBox picBoxKenhMauMagenta;
+        private System.Windows.Forms.PictureBox picBoxKenhMauYellow;
+        private System.Windows.Forms.PictureBox picBoxKenhMauBlack;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 21146033_NGUYENTRUNGNHAN_mini-project6/bai6/bai6/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Load hình .jpg từ file
+
+            Bitmap HinhGoc = new Bitmap(@"D:\XLA\lena_color.png");
+
+            // Cho hiển thị trên pictureBox 
+            picBoxHinhGoc.Image = HinhGoc;
+
+            // Hiển thị các kênh màu CMYK được chuyển đổi từ RGB
+            //Gọi hàm chuyển đổi RGB sang CMYK
+
+
+            List<Bitmap> CMYK = ChuyenDoiRGBSangCMYK(HinhGoc);
+
+            //Hàm trên trả về 4 màu tương ứng thứ tự 0-3 là CMYK
+
+            picBoxKenhMauCyan.Image = CMYK[0];           //Kênh màu Cyan
+            picBoxKenhMauMagenta.Image = CMYK[1];        //Kênh màu Magenta
+            picBoxKenhMauYellow.Image = CMYK[2];         //Kênh màu Yellow
+            picBoxKenhMauBlack.Image = CMYK[3];          //Kênh màu Black
+        }
+
+        public List<Bitmap> ChuyenDoiRGBSangCMYK(Bitmap hinhgoc)
+
+        {
+            // Màu Cyan (xanh dương) là kết hợp giữa Green và Blue , set kênh Red
+            //Màu Magenta(tím) là  kết hợp giữa Red và Blue , set kênh Green
+            //Màu Yellow(vàng) là  kết hợp giữa Red và Green , set kênh Blue
+            //Màu Black(đen) là  lấy MIN(R,G,B)
+
+
+
+            // Tạo 1 list để chứa 4 kênh ảnh tương ứng CMYK
+            //c#.Net kiểu List là 1 mảng nhưng ko cần phải khai báo trước
+
+
+            List<Bitmap> CMYK = new List<Bitmap>();
+
+            // Tạo 4 hình bitmap, kích thước 4 khung bằng với kích thước gốc để chuyển đổi kênh màu đc thực hiện đúng từng pixel
+            //Mỗi kênh trong ko gian màu CMYK được hiển thị bởi 1 hình bitmap
+
+
+            Bitmap Cyan = new Bitmap(hinhgoc.Width, hinhgoc.Height);
+            Bitmap Magenta = new Bitmap(hinhgoc.Width, hinhgoc.Height);
+            Bitmap Yellow = new Bitmap(hinhgoc.Width, hinhgoc.Height);
+            Bitmap Black = new Bitmap(hinhgoc.Width, hinhgoc.Height);
+
+            //Dùng 2 vòng for quét pixel của hình gốc
+
+
+            for (int x = 0; x < hinhgoc.Width; x++)
+                for (int y = 0; y < hinhgoc.Height; y++)
+                {
+                    // Lấy điểm ảnh . Tại mỗi điểm ảnh sẽ tiến hành lấy thông tin 
+                    //Do là ảnh gốc RGB nên mỗi pixel sẽ chứa thông tin của 3 kênh màu 
+
+                    Color pixel = hinhgoc.GetPixel(x, y);
+                    //Mỗi kênh màu đc biểu diễn 8 bit , tương ứng 1 byte 
+                    //Dùng kiểu dữ liệu byte để chứa thông tin từng kênh màu
+
+                    byte R = pixel.R;
+                    byte G = pixel.G;
+                    byte B = pixel.B;
+
+                    // Trộn các kênh màu RGB để cho ra CYMK
+
+                    // Màu Cyan (xanh dương) là kết hợp giữa Green và Blue , set kênh Red=0
+                    Cyan.SetPixel(x, y, Color.FromArgb(0, G, B));
+
+                    //Màu Magenta(tím) là  kết hợp giữa Red và Blue , set kênh Green=0
+                    Magenta.SetPixel(x, y, Color.FromArgb(R, 0, B));
+
+                    //Màu Yellow(vàng) là  kết hợp giữa Red và Green , set kênh Blue=0
+                    Yellow.SetPixel(x, y, Color.FromArgb(R, G, 0));
+
+                    //Màu Black(đen) là  lấy MIN(R,G,B)
+                    //Hàm MIN chỉ có 2 đầu vào nên phải thực hiện 2 lần
+
+                    byte K = Math.Min(R, Math.Min(G, B));
+                    Black.SetPixel(x, y, Color.FromArgb(K, K, K));
+                }
+
+            //Add các hình ảnh tương ứng các kênh màu CMYK vào list
+            //List là kiểu dữ liệu mảng (Array) ko cần bít kích thước nên có thể Add các element 
+            CMYK.Add(Cyan);
+            CMYK.Add(Magenta);
+            CMYK.Add(Yellow);
+            CMYK.Add(Black);
+
+            // Hàm trả về là 1 list 4 ảnh bitmap tương ứng 4 kênh màu CMYK 
+            return CMYK;
+        }
+    }
+}
+
